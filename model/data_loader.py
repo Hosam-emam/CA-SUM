f8f0a1a2d587d9c174f145563b58cd08f58a7d2f
Hosam_Hazem <hosamzoma77@gmail.com>
2026-06-10 22:21:13 +0300

    feat: use Swin Base For training

--- model/data_loader.py
+++ model/data_loader.py
@@ -20,8 +20,8 @@ class VideoData(Dataset):
         #                  '.../CA-SUM/data/TVSum/eccv16_dataset_tvsum_google_pool5.h5']
         # self.splits_filename = ['.../CA-SUM/data/splits/' + self.name + '_splits.json']
 
-        self.datasets = ['/kaggle/input/datasets/michaelnabil88/tvsum-and-summe-feature-extraction/efficient_v2_small/summe_efficient_v2_small_features.h5',
-                         '/kaggle/input/datasets/michaelnabil88/tvsum-and-summe-feature-extraction/efficient_v2_small/tvsum_efficient_v2_small_features.h5']
+        self.datasets = ['/kaggle/input/datasets/michaelnabil88/tvsum-and-summe-feature-extraction/swin/swin_b/summe_swin_b_features.h5',
+                         '/kaggle/input/datasets/michaelnabil88/tvsum-and-summe-feature-extraction/swin/swin_b/tvsum_swin_b_features.h5']
         self.splits_filename = ['/kaggle/working/CA-SUM/data/splits/' + self.name + '_splits.json']
         self.split_index = split_index
 
